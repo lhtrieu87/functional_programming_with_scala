4f1501f85c073622dbd1e818762185c245fa7adc
lhtrieu87 <lhtrieu87@gmail.com>
2014-10-10 23:33:12 +0800

    First example with unit tests

--- example/src/test/scala/example/ListsSuite.scala
+++ example/src/test/scala/example/ListsSuite.scala
@@ -19,7 +19,7 @@ import org.scalatest.junit.JUnitRunner
  * inside eclipse using the built-in JUnit test runner.
  *
  * You have two options for running this test suite:
- * 
+ *
  * - Start the sbt console and run the "test" command
  * - Right-click this file in eclipse and chose "Run As" - "JUnit Test"
  */
@@ -37,8 +37,7 @@ class ListsSuite extends FunSuite {
    * which tests that its argument evaluates to `true`. So one of the simplest
    * successful tests is the following:
    */
-  test("one plus one is two")(assert(1 + 1 == 2))
-
+  test("one plus one is two")(assert(1 + 1 === 2))
 
   /**
    * In Scala, it is allowed to pass an argument to a method using the block
@@ -47,9 +46,10 @@ class ListsSuite extends FunSuite {
    * This allows tests to be written in a more readable manner:
    */
   test("one plus one is three?") {
-    assert(1 + 1 == 3) // This assertion fails! Go ahead and fix it.
+    assert {
+      1 + 1 != 3
+    }
   }
-
 
   /**
    * One problem with the previous (failing) test is that ScalaTest will
@@ -72,9 +72,8 @@ class ListsSuite extends FunSuite {
    * We recommend to always use the `===` equality operator when writing tests.
    */
   test("details why one plus one is not three") {
-    assert(1 + 1 === 3) // Fix me, please!
+    assert(1 + 2 === 3) // Fix me, please!
   }
-
 
   /**
    * In order to test the exceptional behavior of a methods, ScalaTest offers
@@ -94,14 +93,12 @@ class ListsSuite extends FunSuite {
     else x
   }
 
-
   /**
    * Now we finally write some tests for the list functions that have to be
    * implemented for this assignment. We fist import all members of the
    * `List` object.
-   */ 
+   */
   import Lists._
-  
 
   /**
    * We only provide two very basic tests for you. Write more tests to make
@@ -115,10 +112,24 @@ class ListsSuite extends FunSuite {
    * every tested aspect of a method.
    */
   test("sum of a few numbers") {
-    assert(sum(List(1,2,0)) === 3)
+    assert(sum(List(1, 2, 0)) === 3)
   }
-  
+
+  test("sum of an empty list") {
+    assert(sum(List()) === 0)
+  }
+
   test("max of a few numbers") {
     assert(max(List(3, 7, 2)) === 7)
+  }
+
+  test("max of an empty list") {
+    intercept[NoSuchElementException] {
+      max(List())
+    }
+  }
+
+  test("max of a list of one element") {
+    assert(max(List(10)) === 10)
   }
 }
